dc732331dbe6b6c8a6626433777bd5af9897326f
Ismael Pérez Martín <ismaelprzm@gmail.com>
2017-12-20 16:50:19 +0100

    Añado las interfaces

--- src/test/java/uva/tds/pr2/equipo05/GDSecuenceTest.java
+++ src/test/java/uva/tds/pr2/equipo05/GDSecuenceTest.java
@@ -3,12 +3,14 @@ package uva.tds.pr2.equipo05;
 import static org.junit.Assert.*;
 
 import org.junit.Test;
+import org.junit.experimental.categories.Category;
 
 /**
  * Implementacion de los test de secuencia de la clase GD
  * @author ismpere
  * @author martorb
  */
+@Category({Unit.class,TDD.class})
 public class GDSecuenceTest {
 	
 	private static final double ERROR_ADMISIBLE = 0.01;
