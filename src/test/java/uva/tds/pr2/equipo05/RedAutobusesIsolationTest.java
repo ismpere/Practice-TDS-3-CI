4eb3c2f8678671fa9c2629f0da7acfb86f4cc3b1
Mario <mariotorbado.96@gmail.com>
2017-12-27 20:14:40 +0100

    Añadouna nueva clase para los test de isolation de red

---
+++ src/test/java/uva/tds/pr2/equipo05/RedAutobusesIsolationTest.java
@@ -0,0 +1,23 @@
+package uva.tds.pr2.equipo05;
+ 
+import static org.junit.Assert.*;
+ 
+import static org.easymock.EasyMock.createMock;
+import static org.easymock.EasyMock.eq;
+import static org.easymock.EasyMock.expect;
+import static org.easymock.EasyMock.replay;
+import static org.easymock.EasyMock.verify;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.experimental.categories.Category;
+ 
+@Category({Isolation.class})
+public class RedAutobusesIsolationTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
