29c29de0900a39fdb8d35e7fe91120d6a53043d4
Ismael Pérez Martín <ismaelprzm@gmail.com>
2017-12-27 01:23:40 +0100

    Añado otro test de aislamiento de Linea con respecto a Parada y GD

--- src/test/java/uva/tds/pr2/equipo05/LineaIsolationTest.java
+++ src/test/java/uva/tds/pr2/equipo05/LineaIsolationTest.java
@@ -193,5 +193,74 @@ public class LineaIsolationTest {
 		
 		verify(p2); verify(p3); verify(gd4);
 	}
+	
+	@Test
+	public void testGetParadasConCorrespondenciaValido(){
+		Parada p4 = createMock(Parada.class);
+		Parada p5 = createMock(Parada.class);
+		
+		expect(p1.getDistanciaEntre(eq(p4))).andReturn((double)0.0).times(2);
+		expect(p1.getDistanciaEntre(eq(p5))).andReturn((double)0.0).once();
+		expect(p1.getDistanciaEntre(eq(p3))).andReturn((double)0.0).once();
+		expect(p2.getDistanciaEntre(eq(p4))).andReturn((double)0.0).times(2);
+		expect(p2.getDistanciaEntre(eq(p5))).andReturn((double)0.0).once();
+		expect(p2.getDistanciaEntre(eq(p3))).andReturn((double)0.0).once();
+		expect(p3.getDistanciaEntre(eq(p4))).andReturn((double)0.0).times(2);
+		expect(p3.getDistanciaEntre(eq(p5))).andReturn((double)0.0).once();
+		expect(p3.getDistanciaEntre(eq(p3))).andReturn((double)0.0).once();
+		
+		expect(p4.getDistanciaEntre(eq(p3))).andReturn((double)10.00).once();
+		
+		replay(p1); replay(p2); replay(p3); replay(p4);
+		
+		Linea l1 = new Linea(1, p);
+		
+    	Parada pa3[] = {p4, p5, p3};
+		Linea l2 = new Linea(2, pa3);
+		
+		Parada pa2[] = l1.getParadasConCorrespondencia(l2);
+		
+		Parada pcc[] = {p1, p2, p3};
+		
+		assertNotNull(l1);
+		assertNotNull(l2);
+		assertNotNull(pa2);
+		assertTrue(l1.existeCorrespondencia(l2));
+		assertArrayEquals(pcc, pa2);
+		
+		verify(p2); verify(p3); verify(p4);
+	}
+
+	@Test
+	public void testGetParadasConTransbordoDirectoValido(){
+		Parada p5 = createMock(Parada.class);
+		GD gd1 = createMock(GD.class);
+		GD gd2 = createMock(GD.class);
+		GD gd3 = createMock(GD.class);
+		
+		expect(p1.getDistanciaEntre(eq(p3))).andReturn((double)10.00).once();
+		expect(p1.getGD()).andReturn((GD)gd1).times(10);
+		expect(p2.getGD()).andReturn((GD)gd2).times(6);
+		expect(p3.getGD()).andReturn((GD)gd1).times(8);
+		expect(p5.getGD()).andReturn((GD)gd3).times(4);
+		
+		replay(p1); replay(p2); replay(p3); replay(p5);
+		
+		Linea l1 = new Linea(1, p);
+		
+		Parada pa3[] = {p1, p5, p3};
+		Linea l2 = new Linea(2, pa3);
+		
+		Parada pa2[] = l1.getParadasConTransbordoDirecto(l2);
+		
+		Parada pct[] = {p1, p3};
+		
+		assertNotNull(l1);
+		assertNotNull(l2);
+		assertNotNull(pa2);
+		assertTrue(l1.existeTransbordoDirecto(l2));
+		
+		verify(p2); verify(p3); verify(p5);
+	}
 
 }
